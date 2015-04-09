e0c92e95fde4c273e29b4068af9ff273a7341ead
ixixii <ixixii@sina.cn>
2015-04-09 11:20:16 +0800

    a ALMoviePlayer Demo
    ALMoviePlayer is a open sourced third media player ,based on
    MPPlayer，support diy controls UI and orientation

---
+++ ALPlayerDeno/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALPlayerDeno
+//
+//  Created by xss on 15/4/9.
+//  Copyright (c) 2015年 beyond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ALPlayerDeno/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALPlayerDeno
+//
+//  Created by xss on 15/4/9.
+//  Copyright (c) 2015年 beyond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
